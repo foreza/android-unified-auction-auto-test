8532f8746f2e95290ea959b5f93936833e4dfc24
Jason Chiu <jason.chiu@inmobi.com>
2020-03-09 13:25:04 -0700

    Update all test constants to match sources set up on the SSUI

--- app/src/main/java/com/inmobise/myapplication/TestConstants.java
+++ app/src/main/java/com/inmobise/myapplication/TestConstants.java
@@ -5,15 +5,33 @@ public class TestConstants {
     public static final String LOG_TAG = "UnifiedAuctionTest";
 
 
-    public static final String default_test_app_id = "1022221"; // 1022422
+    public static final String default_test_app_id = "1022221";
 
     // Non-mediation
-    public static final String default_test_interstitial_id = "1069151"; // "1069521";
+    public static final String default_test_interstitial_id = "1069151";
     public static final String default_test_320x50_id = "1069149";
     public static final String default_test_mrec_id = "1069150";
 
+    // All mediation
+    public static final String full_test_interstitial_id = "1070701";
+    public static final String full_test_320x50_id = "1070700";
+    public static final String full_test_mrec_id = "1070698";
 
+    // AdMob Mediation
+    public static final String admob_test_320x50 = "1069144";
+    public static final String admob_test_mrec = "1069145";
+    public static final String admob_test_int = "1069146";
 
+    // Facebook Mediation
+    public static final String fb_test_320x50 = "1069156";
+    public static final String fb_test_mrec = "1069157";
+    public static final String fb_test_int = "1069148";
+
+    // AdColony Mediation
+    public static final String adcolony_test_int = "1069152";
+
+    // Vungle Mediation
+    public static final String vungle_test_int = "1069153";
 
 
     public static final String test_metric_endpoint = "http://107.170.192.117:8900/api/session/";
